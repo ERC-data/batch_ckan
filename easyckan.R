e267314453d08796ca2ad72fa7093a453632d4cc
SaintlyVi <wiebke.toussaint@gmail.com>
2016-11-08 17:53:19 +0200

    first commit

---
+++ easyckan.R
@@ -0,0 +1,21 @@
+library(ckanr)
+
+mypackage <- 'tp2-model-outputs'
+ckanr_setup(url = 'http://energydata.uct.ac.za', key = readline(prompt = 'enter your apikey: '))
+
+batchdelete <- function(){
+  rsrcs <- package_show(mypackage, as = 'table')$resources['id']
+  for (r in rsrcs$id){
+    resource_delete(r)
+  }
+}
+
+batchcreate <- function(){
+  path <- readline(prompt = 'enter to resource folder: ')
+  files <- dir(path)
+  for (f in files){
+    filepath <- paste(path, f, sep = '/')
+    print(filepath)
+    resource_create(package_id = mypackage, name = f, upload = filepath)
+  }
+}
