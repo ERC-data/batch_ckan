22cae341659c640fab272dd29b4ff91313304199
SaintlyVi <wiebke.toussaint@gmail.com>
2016-11-22 18:26:57 +0200

    fine tuning resources descriptions

--- easyckan.R
+++ easyckan.R
@@ -1,22 +1,22 @@
 library(ckanr)
 
-mypackage <- 'tp2-model-outputs'
-ckanr_setup(url = 'http://energydata.uct.ac.za', key = readline(prompt = 'enter your apikey: '))
+ckanr_setup(url = 'http://energydata.uct.ac.za', key = readline(prompt = 'Enter your apikey: \n'))
 
-batchdelete <- function(){
-  rsrcs <- package_show(mypackage, as = 'table')$resources['id']
+batchdelete <- function(dataset = readline('Enter a valid dataset id: \n')){
+  rsrcs <- package_show(dataset, as = 'table')$resources['id']
   for (r in rsrcs$id){
     resource_delete(r)
   }
 }
 
-batchcreate <- function(){
+batchcreate <- function(dataset = readline('Enter a valid dataset id: \n')){
   path <- readline(prompt = 'enter path to resource folder: ')
   files <- dir(path)
   for (f in files){
     filepath <- paste(path, f, sep = '/')
     filename <- toupper(gsub("\\..*", "",f))
+    dscrptn <- attr(readRDS(filepath), 'comment') ###create a description function?
     print(paste('...creating ',filepath))
-    resource_create(package_id = mypackage, name = filename, upload = filepath)
+    resource_create(package_id = dataset, description = dscrptn, name = filename, upload = filepath)
   }
 }
