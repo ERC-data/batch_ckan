9a5fc72f16a780f3dc568c8a0739e0a216e70035
SaintlyVi <wiebke.toussaint@gmail.com>
2016-11-23 17:28:41 +0200

    added function for automatic descriptions

--- easyckan.R
+++ easyckan.R
@@ -1,22 +1,46 @@
 library(ckanr)
-
+  
 ckanr_setup(url = 'http://energydata.uct.ac.za', key = readline(prompt = 'Enter your apikey: \n'))
 
-batchdelete <- function(dataset = readline('Enter a valid dataset id: \n')){
-  rsrcs <- package_show(dataset, as = 'table')$resources['id']
-  for (r in rsrcs$id){
-    resource_delete(r)
-  }
+setpath <- function(path){
+    if(missing(path)){
+        if(exists("path", envir = .GlobalEnv)) {
+            print(paste('Your path is set to', get0('path', envir = .GlobalEnv), '. You can choose a different folder with setpath(\'your/path\').'))
+        } else {
+            path <- readline(prompt = 'Enter path to resource folder: ')
+            assign("path", path, envir = .GlobalEnv) 
+        }
+    } else {
+        assign("path", path, envir = .GlobalEnv) 
+    } 
 }
 
-batchcreate <- function(dataset = readline('Enter a valid dataset id: \n')){
-  path <- readline(prompt = 'enter path to resource folder: ')
-  files <- dir(path)
-  for (f in files){
-    filepath <- paste(path, f, sep = '/')
-    filename <- toupper(gsub("\\..*", "",f))
-    dscrptn <- attr(readRDS(filepath), 'comment') ###create a description function?
-    print(paste('...creating ',filepath))
-    resource_create(package_id = dataset, description = dscrptn, name = filename, upload = filepath)
-  }
+description <- function(path, filematch, metadata){
+    if(isTRUE(metadata)){
+        metaformat <- readline('In which format have you saved your metadata file? Choose \'rdsattr\' or \'csv\': ')
+        switch(metaformat,
+               rdsattr = attr(readRDS(path), 'comment'),
+               csv = read.csv(paste(path, 'metadata.csv', sep = '/'), row.names = 'Name', stringsAsFactors = FALSE)[filematch,'Description']
+        )}
+}
+
+batchdelete <- function(dataset = readline('Enter a valid dataset id: \n')){
+    rsrcs <- package_show(dataset, as = 'table')$resources['id']
+    for (r in rsrcs$id){
+        resource_delete(r)
+    }
+}
+
+batchcreate <- function(dataset = readline('Enter a valid dataset id: \n'), metadata){
+    setpath()
+    files <- dir(path)
+    match.arg(as.character(metadata), choices = c(TRUE,FALSE))
+    for (f in files){
+        filepath <- paste(path, f, sep = '/')
+        filename <- toupper(gsub("\\..*", "",f))
+        dscrptn <- description(path, f, metadata)
+        print(dscrptn)
+        print(paste('...creating ',filepath))
+        resource_create(package_id = dataset, description = dscrptn, name = filename, upload = filepath)
+    }
 }
