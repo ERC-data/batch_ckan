4bd5774b73773158b0d89fd859a885f60ee76367
SaintlyVi <wiebke.toussaint@gmail.com>
2016-12-13 15:25:50 +0200

    batchcreate() working

--- easyckan.R
+++ easyckan.R
@@ -5,7 +5,7 @@ ckanr_setup(url = 'http://energydata.uct.ac.za', key = readline(prompt = 'Enter 
 setpath <- function(path){
     if(missing(path)){
         if(exists("path", envir = .GlobalEnv)) {
-            print(paste('Your path is set to', get0('path', envir = .GlobalEnv), '. You can choose a different folder with setpath(\'your/path\').'))
+            # do nothing
         } else {
             path <- readline(prompt = 'Enter path to resource folder: ')
             assign("path", path, envir = .GlobalEnv) 
@@ -13,69 +13,71 @@ setpath <- function(path){
     } else {
         assign("path", path, envir = .GlobalEnv) 
     }
-    files <- dir(get0('path', envir = .GlobalEnv))
-    assign("files", files, envir = .GlobalEnv)
-}
-
-description <- function(path, filematch, metadata){
-    if(isTRUE(metadata)){
-        metaformat <- metadata()
-        switch(metaformat,
-               rdsattr = attr(readRDS(paste(path, filematch, sep = '/')), 'comment'),
-               csv = read.csv(paste(path, 'metadata.csv', sep = '/'), row.names = 'Name', stringsAsFactors = FALSE)[filematch, 'Description']
-        )}
+    all_files <- dir(get0('path', envir = .GlobalEnv))
+    uploads <- all_files[all_files != 'metadata.csv'] #remove metadata file from list of files
+    assign("all_files", all_files, envir = .GlobalEnv)
+    assign("uploads", uploads, envir = .GlobalEnv)
 }
 
 metadata <- function(){
     setpath()
-    if(!is.na(pmatch('metadata', files))){
+    if(!is.na(pmatch('metadata', all_files))){
         return('csv')
     }else{
-        print('Could not find a metadata file. Checking for rds files.\n')
+        print('Could not find a metadata file. Checking for rds files.')
         # Check if files are saved as csv or rds files
-        if(sum(grepl('rds', files))/length(files) > 0.8){
+        if(sum(grepl('rds', uploads))/length(uploads) > 0.8){
             return('rdsattr')
         }else{
-            write.table(data.frame('Names' = files, 'Description' = rep("", length(files))), paste(path,'metadata.csv',sep='/'), row.names = FALSE, col.names = TRUE)
-            print('We have created a metadata.csv file for your dataset. Please add descriptions for your resources.\n')
-            break
+            write.csv(data.frame('Names' = uploads, 'Description' = rep("", length(uploads))), paste(path,'metadata.csv',sep='/'), row.names = FALSE)
+            print('We have created a metadata.csv file for your dataset. Please add descriptions for your resources.')
+            return('new')
         }
     }
 }
 
-batchcreate <- function(dataset = readline('Enter a valid dataset id: \n'), metadata){
+description <- function(path = path, metadata = TRUE, filematch){
+    if(isTRUE(metadata)){
+        metaformat <- metadata()
+        switch(metaformat,
+               rdsattr = attr(readRDS(paste(path, filematch, sep = '/')), 'comment'),
+               csv = read.csv(paste(path, 'metadata.csv', sep = '/'), row.names = c('Names'), 
+                              stringsAsFactors = FALSE)[filematch, 'Description'],
+               new = stop("You must add descriptions to your metadata file to continue.")
+        )}
+}
+
+batchcreate <- function(dataset = readline('Enter a valid dataset id: \n'), metadata = TRUE){
     remove(path, envir = .GlobalEnv)
     setpath()
     match.arg(as.character(metadata), choices = c(TRUE,FALSE))
-    files <- files[-match('metadata.csv', files)] #remove metadata file from list of files
-    print(files)
-    for (f in files){
-        filepath <- paste(path, f, sep = '/')
-        filename <- gsub("\\..*", "",f)
-        dscrptn <- description(path, f, metadata)
-        print(paste('...creating ',filename))
-        # create resources and check if creation was successful
-        if (exists('test')){
-            remove(test)
-        }
-        count <- 1
-        while(!exists('test') && count < 5){
-            try({
-                r <- resource_create(package_id = dataset, description = dscrptn, name = filename, upload = filepath)
-                test <- resource_show(r$id)
-                print(test)
-                count <- count + 1
-                Sys.sleep(1)
-                })
-        }
-    }}
+    
+    # Create a dataframe that contains all the details to upload resources to CKAN
+    resources <- data.frame(upload = uploads)
+    resources$filepath <- sapply(resources$upload, function(x) paste(path, x, sep = '/'))
+    resources$filename <- sapply(resources$upload, function(x) gsub("\\..*", "",x))
+    resources$description <- sapply(resources$upload, function(x) description(path, metadata, x))
+    
+    # Upload the resources to CKAN and save the returned ckan object to an R result object
+    result <- sapply(1:NROW(resources), function(r) resource_create(package_id = dataset, description = resources[r,'description'], 
+                                       name = resources[r,'filename'], upload = resources[r,'filepath']))
+    
+    # Check that all resources have uploaded and retry if this is not the case
+    for (i in 1:ncol(result)){
+        detail <- resources[resources$filename == result['name',i],]
+        tryCatch(
+            r <- resource_show(result['id',i]),
+            error = function(e) resource_create(package_id = dataset, description = detail$description, 
+                                                name = detail$filename, upload = detail$filepath),
+            finally = print(r)
+        )
+    }
+    return(list(resources, result))}
 
 batchdelete <- function(dataset = readline('Enter a valid dataset id: \n')){
     rsrcs <- package_show(dataset, as = 'table')$resources['id']
-    for (r in rsrcs$id){
-        resource_delete(r)
+    sapply(rsrcs$id, function(x) resource_delete(x))
     }
-}
 
 private <- function(dataset){
     package_patch(x = list(private='true'), dataset)
